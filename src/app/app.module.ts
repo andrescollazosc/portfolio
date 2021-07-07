384f637dd8a16e6f2bfb99738bac8df630cdd80c
andrescollazosc <andrescollazosc@gmail.com>
2021-07-07 14:13:31 -0500

    Added new project BS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroMainComponent } from './components/hero-main/hero-main.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroMainComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
