c14ac2dc08bbcf38361172abb9384ceaa8aac918
Andres Collazos <andrescollazosc@gmail.com>
2021-08-17 21:41:22 -0500

    Added new CV version

--- src/app/mocks/profile-card.mock.ts
+++ src/app/mocks/profile-card.mock.ts
@@ -5,7 +5,7 @@ export class ProfileCardMock {
     public static getProfile(): ProfileCardModel {
         return {
             title: 'Hi, I am Andres Collazos, Nice to meet you',
-            description: 'I am a systems engineer with more than nine years of experience as a software developer, in my experience I have had the opportunity to work for many sectors in different technologies, I am passionate about good code and a coffee lover..'
+            description: 'I am a systems engineer with more than nine years of experience as a software developer, in my experience I have had the opportunity to work for many sectors in different technologies, I am passionate about good code and a coffee lover.'
         };
     }
 
